6f5abfeaacf4e7274706f1df09687cb560b1f857
Master-Faraz <farazali9028@gmail.com>
2021-12-17 02:17:49 +0530

    java

---
+++ first.java
@@ -0,0 +1,20 @@
+public class first {
+    public static void main(String[] args) {
+
+        System.out.println("Faraz");
+        int a;
+        a=500;
+        System.out.println(a);
+
+        char c = 'C';
+        System.out.println(c);
+
+        boolean condition = false;
+        System.out.println(condition);
+
+        String s = "Faraz ali ";
+        System.out.println(s);
+    
+    }
+
+}
