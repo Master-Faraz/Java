ae0284390084a8885527ad97e6fd02da2003c989
Master-Faraz <farazali9028@gmail.com>
2021-12-17 19:59:49 +0530

    java basics

---
+++ Array.java
@@ -0,0 +1,19 @@
+public class Array {
+    public static void main(String[] args) 
+    {
+        int arr[] = new int[5]; // . new keyword is used when we want to initialize the array
+
+        for (int i = 0; i < arr.length; i++) // arr.length --> obj for finding array length
+        {
+            arr[i] = (i + 1) * 100;
+            System.out.println(arr[i]);
+        }
+
+        // . For 2D array
+
+        
+
+
+
+    }
+}
